8bccdce01e9a20d12b472ccf98131e0a620e6c5e
WhatIsFun <mohammed110051@gmail.com>
2023-06-13 14:55:49 +0400

    Book

---
+++ Book.cs
@@ -0,0 +1,23 @@
+﻿
+namespace cSharp_OOP_Practices
+{
+    internal class Book
+    {
+        public string Title;
+        public string Description;
+        public string Author;
+        public int Year;
+
+        public Book (string title, string description, string Auther, int Year)
+        {
+            this .Title = title;
+            this .Description = description;
+            this .Author = Auther;
+            this .Year = Year;
+
+        }
+
+        
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace cSharp_OOP_Practices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Book book = new Book("Funny Stuff", " ", "WhatIsFun", 1997);
+            Console.WriteLine("Book details> ");
+            Console.WriteLine("Book title: "+ book.Title);
+            Console.WriteLine("Book description: "+ book.Description);
+            Console.WriteLine("Book Auther: "+ book.Author);
+            Console.WriteLine("Book published year: "+ book.Year);
+
+        }
+    }
+}
