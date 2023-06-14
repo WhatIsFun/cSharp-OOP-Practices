f4cbe3d0bb9cfaec8238ede416b22c1bde1d796e
WhatIsFun <mohammed110051@gmail.com>
2023-06-14 09:35:29 +0400

    Circle Area

---
+++ Circle.cs
@@ -0,0 +1,19 @@
+﻿
+namespace cSharp_OOP_Practices
+{
+    class Circle
+    {
+        public double radius;
+        public double PI = 3.14;
+        public double circleArea = 0;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public double getArea()
+        {
+            return circleArea = PI * radius * radius;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,14 +6,27 @@ namespace cSharp_OOP_Practices
     {
         static void Main(string[] args)
         {
+            /*Create a Book class with the following properties:
+            Title(string)
+            Author(string)
+            Year(int)
+            Implement the default constructor and a parameterized constructor that takes values for all the properties.*/
 
-            Book book = new Book("Funny Stuff", " ", "WhatIsFun", 1997);
-            Console.WriteLine("Book details> ");
-            Console.WriteLine("Book title: "+ book.Title);
-            Console.WriteLine("Book description: "+ book.Description);
-            Console.WriteLine("Book Auther: "+ book.Author);
-            Console.WriteLine("Book published year: "+ book.Year);
+            //Book book = new Book("Funny Stuff", " ", "WhatIsFun", 1997);
+            //Console.WriteLine("Book details> ");
+            //Console.WriteLine("Book title: "+ book.Title);
+            //Console.WriteLine("Book description: "+ book.Description);
+            //Console.WriteLine("Book auther: "+ book.Author);
+            //Console.WriteLine("Book published year: "+ book.Year);
 
+            /*Create a Circle class with the following properties:
+            Radius (double)
+            Implement the default constructor and a parameterized constructor 
+            that takes the radius as a parameter. Additionally, implement a method 
+            to calculate and return the area of the circle.*/
+
+            Circle circle = new Circle(2);
+            Console.WriteLine("The circle area of radius " +  circle.radius + " is "+ circle.getArea());
         }
     }
 }
